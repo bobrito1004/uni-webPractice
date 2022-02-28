ec4a6c151dd8955e14ff7ec9aa3d07dbd2c88613
bobrito1004 <AlexLex2001@gmail.com>
2022-02-28 18:36:48 +0300

    port fixed

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('pug');
-  await app.listen(766);
+  const PORT = process.env.PORT || 12345;
+  await app.listen(PORT);
 }
 bootstrap();
